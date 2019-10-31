731fdcd448405ddc6f643a62ec1b95e6d16d2fb0
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-10-31 18:39:43 +0000

    Add fabric as a helper to deploy

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import env, local, cd, run, task
+from prettyconf import config
+
+env.hosts = config('DEPLOY_MACHINES', cast=config.list)
+
+
+@task
+def deploy():
+    local('git push')
+    with cd('~/pylique'):
+        run('git pull')
+        run('pipenv install')
+        run('supervisorctl restart pylique')
